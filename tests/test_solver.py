15af202c6db4af9190f4d2a318ed0d05e1066bc7
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-16 23:38:54 -0700

    towards Markowitz

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -8,31 +8,6 @@ import pandas as pd
 from cvx.markowitz.solver import Solver
 
 
-class TestSolver(Solver):
-    """Max Sharpe solver"""
-
-    def add_constraints(self, **kwargs):
-        # self.constraints["Risk"] = self.variance() <= kwargs.get("Risk", 1.0)
-        # self.constraints["Lower"] = self.funding * self.lower_bound <= self.weights
-        # self.constraints["Upper"] = self.funding * self.upper_bound >= self.weights
-        # exclude the zero solution...
-        # self.constraints["Investment"] = self.funding >= 0.1
-
-        # self.constraints["Lower"] = self.lower_bound <= self.weights
-        # self.constraints["Upper"] = self.upper_bound >= self.weights
-        self.constraints["Funding"] = self.funding == 1.0
-        self.constraints["Long Only"] = self.weights >= 0
-
-    def objective(self, **kwargs):
-        return cp.Maximize(self.weights @ np.array([1, 2, 3]))
-
-    # def objective(self, **kwargs):
-    #    return cvx.Maximize(self.expected_return)
-
-    def solve(self, **kwargs):
-        return self._solve(scale=True, **kwargs)
-
-
 def test_solver():
     solver = Solver(assets=["a", "b", "c"])
     solver.objective = cp.Maximize(solver.weights @ np.array([1, 2, 3]))

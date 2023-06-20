540e706a1294510fb00476df09e5ef0ac789cf1f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-20 15:17:05 -0700

    simple MaxSharpe routine

---
+++ experiments/maxSharpe.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import cvxpy as cp
+
+from cvx.risk.factor import FactorModel
+from cvx.risk.linalg import pca
+
+if __name__ == '__main__':
+    # create a cvxpy problem
+    prices = pd.read_csv("data/stock_prices.csv", index_col=0, header=0, parse_dates=True)
+    returns = prices.pct_change().dropna(axis=0, how='all')
+
+    # compute 10 components
+    components = pca(returns=returns, n_components=10)
+
+    model = FactorModel(assets=20, k=10)
+
+    lower = np.zeros(20)
+    upper = np.ones(20)
+
+    model.update_data(
+        cov=components.cov.values,
+        exposure=components.exposure.values,
+        idiosyncratic_risk=components.idiosyncratic.std().values,
+        lower=lower,
+        upper=upper
+    )
+
+    weights = cp.Variable(20)
+    factor_weights = cp.Variable(10)
+
+    mu = np.random.rand(20) / 100.0
+
+    objective = cp.Minimize(-mu @ weights + cp.pos(model.estimate_risk(weights, y=factor_weights) - 0.01))
+    constraints = [cp.sum(weights) == 1.0, weights >= 0, factor_weights == model.exposure @ weights]
+
+    problem = cp.Problem(objective, constraints)
+    problem.solve()
+    print(weights.value)
+    print(np.sum(weights.value))
+    weights = pd.Series(index=returns.columns, data=weights.value)
+    print(weights)
+    print(model.estimate_risk(weights.values).value)
+
+    # fill in data

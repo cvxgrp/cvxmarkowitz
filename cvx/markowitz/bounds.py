18bd246c9a5ee0b5256c0f98750c80fbc2ffa5a8
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-21 17:15:09 -0700

    testing bounds

--- cvx/markowitz/bounds.py
+++ cvx/markowitz/bounds.py
@@ -1,33 +1,53 @@
 # -*- coding: utf-8 -*-
-"""
-Bounds
-"""
+"""Bounds"""
 from __future__ import annotations
 
 from dataclasses import dataclass
 
+import cvxpy as cp
 import numpy as np
 
+from .model import Model
 
-@dataclass(frozen=True)
-class Bounds:
-    lower: float = -np.infty
-    upper: float = +np.infty
+
+@dataclass
+class Bounds(Model):
+    m: int = 0
+    name: str = ""
+
+    def estimate(self, weights, **kwargs):
+        """No estimation for bounds"""
+        raise NotImplementedError("No estimation for bounds")
+
+    def _f(self, str):
+        return f"{str}_{self.name}"
 
     def __post_init__(self):
-        assert (
-            self.lower <= self.upper
-        ), "lower bound must be less than or equal to upper bound"
+        self.parameter[self._f("lower")] = cp.Parameter(
+            shape=self.m,
+            name="lower bound",
+            value=np.zeros(self.m),
+        )
+        self.parameter[self._f("upper")] = cp.Parameter(
+            shape=self.m,
+            name="upper bound",
+            value=np.ones(self.m),
+        )
 
+    def update(self, **kwargs):
+        # lower = kwargs.get("lower", np.zeros(self.m))
+        lower = kwargs[self._f("lower")]
+        self.parameter[self._f("lower")].value = np.zeros(self.m)
+        self.parameter[self._f("lower")].value[: len(lower)] = lower
 
-if __name__ == "__main__":
-    assets = ["AAPL", "MSFT"]
-    lower_bound = {"AAPL": 0.0, "MSFT": 0.0}
-    upper_bound = {"AAPL": 1.0, "MSFT": 1.0}
+        upper = kwargs[self._f("upper")]  # .get("upper", np.ones(self.m))
+        self.parameter[self._f("upper")].value = np.zeros(self.m)
+        self.parameter[self._f("upper")].value[
+            : len(upper)
+        ] = upper  # kwargs.get("upper", np.ones(m))
 
-    bounds = {
-        asset: Bounds(lower=lower_bound[asset], upper=upper_bound[asset])
-        for asset in assets
-    }
-
-    print(bounds)
+    def constraints(self, weights, **kwargs):
+        return [
+            weights >= self.parameter[self._f("lower")],
+            weights <= self.parameter[self._f("upper")],
+        ]
---
+++ tests/test_markowitz/test_bounds.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import cvxpy as cp
+import numpy as np
+import pytest
+
+from cvx.markowitz.bounds import Bounds
+
+
+def test_raise_not_implemented():
+    weights = cp.Variable(3)
+    bounds = Bounds(m=3, name="assets")
+
+    with pytest.raises(NotImplementedError):
+        bounds.estimate(weights)
+
+
+def test_constraints():
+    weights = cp.Variable(3)
+    bounds = Bounds(m=3, name="assets")
+    bounds.update(
+        lower_assets=np.array([0.1, 0.2]), upper_assets=np.array([0.3, 0.4, 0.5])
+    )
+
+    assert bounds.parameter["lower_assets"].value == pytest.approx(
+        np.array([0.1, 0.2, 0])
+    )
+    assert bounds.parameter["upper_assets"].value == pytest.approx(
+        np.array([0.3, 0.4, 0.5])
+    )
+
+    assert len(bounds.constraints(weights)) == 2

b659b44ead469e7527a60cecd067fc54b1dbb0aa
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-25 20:09:39 +0400

    6 include return expectations (#18)
    * introduce expected returns
    
    * introduce trading costs
    
    * introduce expected returns (#17) (#19)

---
+++ cvx/markowitz/models/trading_costs.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+import cvxpy as cp
+import numpy as np
+
+from cvx.markowitz import Model
+
+
+@dataclass
+class TradingCosts(Model):
+    def __post_init__(self):
+        self.parameter["power"] = cp.Parameter(shape=1, name="power", value=np.ones(1))
+
+        self.data["weights"] = cp.Parameter(
+            shape=self.assets, name="weights", value=np.zeros(self.assets)
+        )
+
+    def estimate(self, weights, **kwargs):
+        # x = weights - self.data["weights"]
+        # print(cp.abs(x).value)
+        # print(cp.sum(cp.power(cp.abs(x), p=self.parameter["power"])).value)
+        return cp.sum(
+            cp.power(cp.abs(weights - self.data["weights"]), p=self.parameter["power"])
+        )
+
+    def update(self, **kwargs):
+        w = kwargs["weights"]
+        n = w.shape[0]
+        self.data["weights"].value = np.zeros(self.assets)
+        self.data["weights"].value[:n] = w
+
+    def constraints(self, weights, **kwargs):
+        raise NotImplementedError("No constraints for trading costs")
---
+++ tests/test_markowitz/test_models/test_trading_costs.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import cvxpy as cp
+import numpy as np
+import pytest
+
+from cvx.markowitz.models.trading_costs import TradingCosts
+
+
+def test_trading_costs():
+    assets = 3
+    model = TradingCosts(assets=assets)
+    model.update(weights=np.array([0.1, 0.2]))
+
+    # weights not explicitly set are zero
+    assert model.data["weights"].value == pytest.approx(np.array([0.1, 0.2, 0.0]))
+
+    # here it's important that the weights
+    weights = cp.Variable(assets)
+    weights.value = np.array([0.4, 0.7, 0.0])
+
+    assert model.estimate(weights).value == pytest.approx(0.8)

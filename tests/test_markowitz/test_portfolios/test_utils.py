7afc060e1069cfd3a02b4261cabd0f7380005993
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-05 22:58:34 -0700

    test approx and test a mismatch

--- tests/test_markowitz/test_portfolios/test_utils.py
+++ tests/test_markowitz/test_portfolios/test_utils.py
@@ -12,15 +12,23 @@ def test_approx():
 
     weights = cp.Variable(5)
 
-    d = dict(approx("xxx", row @ weights, 2.0, 0.1))
+    d = dict(approx("xxx", row @ weights, 0.0, 0.1))
 
     # I want this test!
     x = {
-        "xxx_approx_upper": row @ weights - 2.0 <= 0.1,
-        "xxx_approx_lower": row @ weights - 2.0 >= -0.1,
+        "xxx_approx_upper": row @ weights <= 0.1,
+        "xxx_approx_lower": row @ weights >= -0.1,
     }
+
+    # assert d==x
 
     assert set(d.keys()) == {"xxx_approx_upper", "xxx_approx_lower"}
 
+    weights.value = np.array([0.2, 0.3, -0.29, 0.1, 0.3])
     for c1, c2 in zip(d.values(), x.values()):
-        assert str(c1.tree_copy()) == str(c2.tree_copy())
+        assert c1.value() == c2.value()
+
+    weights.value = np.array([0.2, 0.3, -0.15, 0.1, 0.3])
+    for c1, c2 in zip(d.values(), x.values()):
+        assert c1.value() == c2.value()
+

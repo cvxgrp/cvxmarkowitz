eebaa0af5b6672289b8d8adeccbacfc9137ceabb
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-09 09:53:51 -0700

    aux package for cli

---
+++ cvx/cli/aux/json.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import json
+from collections.abc import Iterable
+
+import numpy as np
+
+
+class _NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def read_json(json_file):
+    with open(json_file, "r") as f:
+        json_data = json.load(f)
+        for name, data in json_data.items():
+            if isinstance(data, Iterable):
+                yield name, np.asarray(data)
+            else:
+                yield name, data
+
+
+def write_json(json_file, data):
+    with open(json_file, "w") as f:
+        json.dump(data, f, cls=_NumpyEncoder)

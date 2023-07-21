82c71c1ff3e77a032ed58774a850d9571bf47b2a
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-21 09:40:09 -0700

    test cholesky

--- tests/test_linalg/test_cholesky.py
+++ tests/test_linalg/test_cholesky.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import numpy as np
+import pytest
 
 from cvx.linalg import cholesky
 from cvx.linalg.random import rand_cov
@@ -12,3 +13,15 @@ def test_cholesky():
     u = cholesky(a)
     # test numpy arrays are equivalent
     assert np.allclose(a, np.transpose(u) @ u)
+
+
+def test_cholesky_not_symmetric():
+    a = np.random.randn(10, 10)
+    with pytest.raises(np.linalg.LinAlgError):
+        cholesky(a)
+
+
+def test_cholesky_not_square():
+    a = np.random.randn(12, 10)
+    with pytest.raises(np.linalg.LinAlgError):
+        cholesky(a)

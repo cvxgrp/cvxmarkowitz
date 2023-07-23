5ffe68b28b76451f2f8f479c451ae5a2a681346a
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-23 07:12:41 -0700

    switch minvariance.py to fire

--- cvx/cli/minvariance.py
+++ cvx/cli/minvariance.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import traceback
 
-import click
+import fire
 
 from cvx.cli.aux.io import exists
 from cvx.cli.aux.json import read_json
@@ -9,26 +9,7 @@ from cvx.markowitz.builder import deserialize
 from cvx.markowitz.portfolios.min_var import MinVar, estimate_dimensions
 
 
-@click.command()
-@click.argument("json_file", type=click.Path(exists=True, dir_okay=False))
-@click.argument("problem_file", type=click.Path(dir_okay=False), required=False)
-@click.option(
-    "--assets",
-    "-a",
-    default=None,
-    type=int,
-    required=False,
-    help="Number of assets",
-)
-@click.option(
-    "--factors",
-    "-f ",
-    default=None,
-    type=int,
-    required=False,
-    help="Number of factors",
-)
-def minvariance(json_file, problem_file=None, assets=None, factors=None) -> None:
+def cli(json_file, problem_file=None, assets=None, factors=None) -> None:
     # parse the json file with input data for the problem
     try:
         input_data = dict(read_json(json_file))
@@ -47,20 +28,27 @@ def minvariance(json_file, problem_file=None, assets=None, factors=None) -> None
                 problem = MinVar(assets=assets, factors=factors).build()
             else:
                 assets, factors = estimate_dimensions(**input_data)
-                click.echo(
+                print(
                     f"Estimated the numbers of assets as {assets} and factors as {factors}"
                 )
                 problem = MinVar(assets=assets, factors=factors).build()
 
             # We have constructed the problem, write it to file if the file has been specified
             if problem_file is not None:
-                click.echo(f"Serializing problem in {problem_file}")
+                print(f"Serializing problem in {problem_file}")
                 problem.serialize(problem_file)
 
         problem.update(**input_data)
         problem.solve()
-        click.echo(f"Solution: {problem.weights}")
+        return problem.weights
 
     except Exception as e:
-        click.echo(traceback.print_exception(type(e), e, e.__traceback__))
+        print(traceback.print_exception(type(e), e, e.__traceback__))
         raise e
+
+
+def main():  # pragma: no cover
+    """
+    Run the CLI using Fire
+    """
+    fire.Fire(cli)

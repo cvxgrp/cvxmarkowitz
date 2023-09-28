d6de68ca3626c3bf27d706537ea9639dc0b56a6b
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-09-27 21:50:54 -0700

    update conf for sphinx

--- book/sphinx/conf.py
+++ book/sphinx/conf.py
@@ -12,9 +12,8 @@ source_suffix = {
 extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
-    "autoapi.extension",
     "sphinx.ext.napoleon",
-    "recommonmark",
+    "autoapi.extension",
 ]
 
 

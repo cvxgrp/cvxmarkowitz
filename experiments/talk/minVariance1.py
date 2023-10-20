bc610a9d7e7ec6b55a6a3123a560b40077e80f61
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-10-20 16:14:30 +0400

    fmt

--- experiments/talk/minVariance1.py
+++ experiments/talk/minVariance1.py
@@ -1,10 +1,11 @@
-from time import sleep
 import timeit
+from time import sleep
+
+
 def f():
     sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     execution_time = timeit.timeit(f, number=5)
     print(f"{execution_time:.6f} seconds")
-
